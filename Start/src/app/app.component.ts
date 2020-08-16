7695ab774d66214b47e688eba31329db07613ea1
Spencer Pope <34104632+spope851@users.noreply.github.com>
2020-08-16 16:54:14 -0400

    Update app.component.ts

--- Start/src/app/app.component.ts
+++ Start/src/app/app.component.ts
@@ -6,6 +6,15 @@ import { FinanceServiceService } from './services/finance-service.service';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
-  title = 'ngFinance';
- 
+   title = 'ngFinance';
+   timeframes = [
+        {id:0, timeframe: 0, label:'1 Day', selected: false},
+        {id:1, timeframe: 4, label:'1 Week', selected: false},
+        {id:2, timeframe: 20, label:'1 Month', selected: false},
+        {id:3, timeframe: 62, label:'3 Months', selected: false},
+        {id:4, timeframe: 125, label:'6 Months', selected: false},
+        {id:5, timeframe: 251, label:'1 Year', selected: false},
+        {id:6, timeframe: 1288, label:'5 Years', selected: false},
+        {id:7, timeframe: undefined, label:'Max', selected: false}
+      ];
 }
