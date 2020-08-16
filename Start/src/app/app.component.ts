a141c4e9f5e2b8b8c632ae8d87726e9d4ab8300d
Spencer Pope <34104632+spope851@users.noreply.github.com>
2020-08-16 17:00:18 -0400

    Update app.component.ts

--- Start/src/app/app.component.ts
+++ Start/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { FinanceServiceService } from './services/finance-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
-   title = 'ngFinance';
-   timeframes = [
+  title = 'ngFinance';
+  timeframes = [
         {id:0, timeframe: 0, label:'1 Day', selected: false},
         {id:1, timeframe: 4, label:'1 Week', selected: false},
         {id:2, timeframe: 20, label:'1 Month', selected: false},
@@ -17,6 +16,31 @@ export class AppComponent implements OnInit{
         {id:6, timeframe: 1288, label:'5 Years', selected: false},
         {id:7, timeframe: undefined, label:'Max', selected: false}
       ];
+  
+  date$ = [];
+  lineData$ = [];
+  barData$ = [];
+  lineCount: number;
+
+  public chartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        id: 'left-y-axis',
+          type: 'linear',
+          position: 'left'
+        }, {
+          id: 'right-y-axis',
+          type: 'linear',
+          position: 'right'
+        }]
+      }
+  };
+  public chartLabels = this.date$;
+  public chartLegend = true;
+  public chartData = [{data: this.lineData$, label: 'Price', yAxisID: 'right-y-axis', type: 'line'},
+                      {data: this.barData$, label: 'Volume', yAxisID: 'left-y-axis'}];
   
   onKeyup(key: Array<any>){
     console.log(key);
