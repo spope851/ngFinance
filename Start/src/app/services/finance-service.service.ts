28c509fb101fba6f10cdcbc99bd0eee7808e9287
Spencer Pope <34104632+spope851@users.noreply.github.com>
2020-08-16 16:55:01 -0400

    Update finance-service.service.ts

--- Start/src/app/services/finance-service.service.ts
+++ Start/src/app/services/finance-service.service.ts
@@ -7,17 +7,4 @@ import { HttpClient } from '@angular/common/http'
 })
 export class FinanceServiceService {
 
-  urlBeg = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='
-  urlEnd = '&apikey=4AE4U7UBEMCL5PUF'
-  dailyBeg = 'https://www.alphavantage.co/query?outputsize=full&function=TIME_SERIES_DAILY&symbol='
-  constructor(private http: HttpClient) { }
-
-  search(symbol:string): Observable<any[]>{
-    return this.http.get<any[]>(`${this.urlBeg}${symbol}${this.urlEnd}`)
-  }
-
-  daily(symbol:string): Observable<any[]>{
-    return this.http.get<any[]>(`${this.dailyBeg}${symbol}${this.urlEnd}`)
-  }
-
 }
